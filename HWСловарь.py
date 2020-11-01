e673022ba6042b9ac5380cf241ae771ddf900fa7
FallG0D <71002332+FallG0D@users.noreply.github.com>
2020-11-01 14:22:40 +0300

    Update HWСловарь.py

--- HWСловарь.py
+++ HWСловарь.py
@@ -1,4 +1,5 @@
-L = input()
-M = int(input())
-V = int(input())
-a = {'name': L, 'age' :M, 'height': V}
+L = input('Имя:')
+M = int(input("Возраст:"))
+V = int(input("Вес:"))
+Q = input("Пол")
+a = {'name': L, 'age' :M, 'height': V, 'sex': Q}
